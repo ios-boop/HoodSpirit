1a0dee3c11f270a1b801b926c2d83addc80477aa
ios-boop <133600140+ios-boop@users.noreply.github.com>
2024-02-24 23:54:58 +0100

    Update SilentAim.lua

--- SilentAim.lua
+++ SilentAim.lua
@@ -1,4 +1,4 @@
- _G.SilentAim = false
+local SilentAim = true
 local LocalPlayer = game:GetService("Players").LocalPlayer
 local Players = game:GetService("Players")
 local Mouse = LocalPlayer:GetMouse()
@@ -10,7 +10,7 @@ local FOV_CIRCLE = Drawing.new("Circle")
 FOV_CIRCLE.Visible = true
 FOV_CIRCLE.Filled = false
 FOV_CIRCLE.Thickness = 0
-FOV_CIRCLE.Transparency = 1
+FOV_CIRCLE.Transparency = 0.4
 FOV_CIRCLE.Color = Color3.new(0, 0, 0)
 FOV_CIRCLE.Radius = _G.FOV
 FOV_CIRCLE.Position = Vector2.new(Camera.ViewportSize.X / 2, Camera.ViewportSize.Y / 2)
@@ -33,12 +33,12 @@ end coroutine.wrap(MoveFovCircle)()
 
 Mouse.KeyDown:Connect(function(KeyPressed)
     if KeyPressed == (_G.AimKey:lower()) then
-        if _G.SilentAim == false then
+        if SilentAim == false then
             FOV_CIRCLE.Color = Color3.new(0, 0, 0)
-            _G.SilentAim = true
+            SilentAim = true
         elseif _G.SilentAim == true then
             FOV_CIRCLE.Color = Color3.new(0, 0, 0)
-            _G.SilentAim = false
+            SilentAim = false
         end
     end
 end)
@@ -48,7 +48,7 @@ oldIndex = hookmetamethod(game, "__index", function(self, Index)
     if self == Mouse and (Index == "Hit") then 
         local Distance = 9e9
         local Targete = nil
-        if _G.SilentAim then
+        if SilentAim then
 
             for _, v in pairs(Players:GetPlayers()) do 
                 if v ~= LocalPlayer and v.Character and v.Character:FindFirstChild("HumanoidRootPart") and v.Character:FindFirstChild("Humanoid") and v.Character:FindFirstChild("Humanoid").Health > 0 then
@@ -72,7 +72,7 @@ oldIndex = hookmetamethod(game, "__index", function(self, Index)
         end
 
         if Targete ~= nil and Targete[Options.Torso] and Targete:FindFirstChild("Humanoid").Health > 0 then
-            if _G.SilentAim then
+            if SilentAim then
                 local ShootThis = Targete[Options.Torso] -- or Options.Head
                 local Predicted_Position = ShootThis.CFrame + (ShootThis.Velocity * _G.Prediction + Vector3.new(0,-1,0)) --  (-1) = Less blatant
                 return ((Index == "Hit" and Predicted_Position))
